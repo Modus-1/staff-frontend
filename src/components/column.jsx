6e90cb1e52b09355c275954ac6bffba3f9e41425
Lukas <lukas100@live.nl>
2022-11-17 14:47:53 +0100

    Orders are movable between columns
    Co-authored-by: TeunMos <TeunMos@users.noreply.github.com>
    Co-authored-by: rmzhen <rmzhen@users.noreply.github.com>

--- src/components/column.jsx
+++ src/components/column.jsx
@@ -1,16 +1,15 @@
-import React from "react";
 import "../styling/column.css";
 import Order from "./order";
 
 export default function Column(props) {
+
   return (
     <div className="column-container">
         <div className="column-header" >
             <h1>{props.text}</h1> 
         </div>
         <div className="column-body">
-            <Order tableNumber={3} orderNumber={1}></Order>
-            <Order tableNumber={69} orderNumber={2}></Order>
+            {props.orders.map((order) => { return <Order statuses={props.statuses} columnIndex={props.index} key={order.orderNumber} changeStatus={props.changeStatus} order={order}/> })}
         </div>
     </div>
   );
--- src/components/order.jsx
+++ src/components/order.jsx
@@ -1,20 +1,52 @@
+import {useEffect} from 'react';
 import "../styling/order.css";
 export default function Order(props) {
+
+    const id = Math.floor(Math.random() * 1000000000);
+    useEffect(() => {
+
+        document.getElementById(id).style.backgroundColor = props.order.color;
+        // props.order.color
+    }, []);
+
+    function SetorderToNextRow() {
+        props.changeStatus(props.order.id, props.statuses[props.columnIndex+1]);
+    }
+
+    function SetorderToPreviousRow() {
+        props.changeStatus(props.order.id, props.statuses[props.columnIndex-1]);
+    }
+
     return (
         <div className="order-container">
-            <div className="order-header">
-                <span className="order-header-text">Order {props.orderNumber}</span>
-                <span className="order-table-text">Table {props.tableNumber}</span>
+            <div  id={id} className="order-header">
+                <span className="order-header-text">Order {props.order.orderNumber}</span>
+                <span className="order-table-text">Table {props.order.tableNumber}</span>
             </div>
-            <div classname="order-body">
-                <div className="order-item-container">
-                    <div className="order-item-name">item1</div>
-                    <div className="order-item-count">10</div>
+            <div className="order-body">
+                <div className="order-body-list">
+                {props.order.items.map((item, index) => {
+                    return (
+                        <div key={index} className="order-item-container">
+                            <div className="order-item-name">{item.name}</div>
+                            <div className="order-item-count">{item.count}</div>
+                        </div>)
+                })}
                 </div>
-                <div className="order-item-container">
-                    <div className="order-item-name">item2</div>
-                    <div className="order-item-count">69</div>
+                <div className="order-btn-container">
+
+                    {props.columnIndex > 0 && 
+                        <button className="order-status-button" onClick={SetorderToPreviousRow}>Back</button>
+                    }
+                    <div className="order-status-button-spacer"></div>
+                    {props.columnIndex < props.statuses.length-1 && 
+                        <button className="order-status-button" onClick={SetorderToNextRow}>Next</button>
+                    }
+
+
                 </div>
+                
+
             </div>
         </div>
     );
