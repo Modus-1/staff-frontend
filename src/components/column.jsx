2cfa8b1e603723169ef138183e4be7f1b6a9cd79
Calvin Zhen <418224@student.fontys.nl>
2022-11-24 16:08:06 +0100

    Tried sorting function

--- src/components/column.jsx
+++ src/components/column.jsx
@@ -2,15 +2,24 @@ import "../styling/column.css";
 import Order from "./order";
 
 export default function Column(props) {
-
   return (
     <div className="column-container">
-        <div className="column-header" >
-            <h1>{props.text}</h1> 
-        </div>
-        <div className="column-body">
-            {props.orders.map((order) => { return <Order statuses={props.statuses} columnIndex={props.index} key={order.orderNumber} changeStatus={props.changeStatus} order={order}/> })}
-        </div>
+      <div className="column-header">
+        <h1>{props.text}</h1>
+      </div>
+      <div className="column-body">
+        {props.orders.map((order) => {
+          return (
+            <Order
+              statuses={props.statuses}
+              columnIndex={props.index}
+              key={order.orderNumber}
+              changeStatus={props.changeStatus}
+              order={order}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
