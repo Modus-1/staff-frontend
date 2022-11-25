05bc0ec575f957598a32363342e7e46dcb2cebe0
Lukas <93530655+LukasJansen100@users.noreply.github.com>
2022-11-25 13:43:32 +0100

    remove dummy data

--- src/App.js
+++ src/App.js
@@ -14,17 +14,6 @@ function App() {
 
 
   useEffect(() => {
-
-    //dummy data
-    // const itemlist1 = [{name:"Pizza", count:1}, {name:"Burger", count:2}, {name:"Frietjuh", count:20}, {name:"Cola", count:3}, {name:"Fanta", count:1}];
-    // const itemlist2 = [{name:"Koffie", count:1}, {name:"Thee", count:6}, {name:"Cola", count:10}];
-    // const itemlist3 = [{name:"Broodje", count:1}, {name:"Croissant", count:3}, {name:"Koekje", count:20}, {name:"blokje kaas", count:10}];
-    
-    // const order1 = { color:getRandomColor(), id: 1, status: statuses[1], tableNumber: 1, orderNumber: 1, items: itemlist1 };
-    // const order2 = { color:getRandomColor(), id: 2, status: statuses[0], tableNumber: 3, orderNumber: 2, items: itemlist2 };
-    // const order3 = { color:getRandomColor(), id: 3, status: statuses[0], tableNumber: 2, orderNumber: 3, items: itemlist3 };
-    // setOrders([order1, order2, order3]);
-
     init();
   }, []);
 
