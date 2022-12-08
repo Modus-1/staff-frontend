1bc672fce774664f6eb1981e86a0de6917108da9
Calvin Zhen <418224@student.fontys.nl>
2022-12-08 15:12:16 +0100

    Start setup waiter page and restructure routing
    Co-authored-by: Lukas <LukasJansen100@users.noreply.github.com>
    Co-authored-by: TeunMos <TeunMos@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { useEffect, useState } from "react";
 import Column from "./components/column";
 import "./styling/App.css";
-import GetActiveOrders, { changeOrderStatus, convertServiceDataToOrderData } from "./services/OrderServices";
+import GetActiveOrders, {
+  changeOrderStatus,
+  convertServiceDataToOrderData,
+} from "./services/OrderServices";
 import getRandomColor from "./services/ColorService";
 import GetDummyData from "./services/DummyDataService";
 import OrderWebSocketService from "./services/OrderWebSocketService";
+import KitchenPage from "./pages/kitchenPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import WaiterPage from "./pages/waiterPage";
 
-export const statuses = ["New", "In progress", "Done"];
+export const statuses = ["New", "In progress", "Ready", "Delivered"];
 
 const orderWebSocket = new OrderWebSocketService();
 
@@ -18,7 +24,7 @@ function App() {
   useEffect(() => {
     orderWebSocket.onNewOrder = (data) => {
       const order = convertServiceDataToOrderData([data])[0];
-      setOrders((prev) => [...prev, order]);
+      setOrders((prev) => sortOrders([...prev, order]));
       setUnfilteredOrders((prev) => [...prev, data]);
     };
 
@@ -27,16 +33,10 @@ function App() {
 
       setOrders((prev) => {
         const newOrders = prev.filter((order) => order.id !== updatedOrder.id);
-        return [...newOrders, updatedOrder];
+        return sortOrders([...newOrders, updatedOrder]);
       });
-
     };
-
-
-
   }, []);
-
-
 
   useEffect(() => {
     GetActiveOrders().then((result) => {
@@ -49,7 +49,7 @@ function App() {
 
     // if you want to use dummy data instead of the api, add REACT_APP_USE_DUMMY_DATA=true to the .env file
     if (process.env.REACT_APP_USE_DUMMY_DATA === "true") {
-      setOrders(GetDummyData(100, 30));
+      //setOrders(GetDummyData(5, 4));
     }
   }, []);
 
@@ -70,16 +70,20 @@ function App() {
   }, [orders]);
 
   function changeStatus(id, status) {
+    console.log("step1");
     const newOrders = orders.map((order) => {
       if (order.id === id) {
+        console.log("step2:");
+        console.log(order);
         let oldIndex = statuses.indexOf(order.status);
         order.status = status;
         let index = statuses.indexOf(status);
         if (
-          (oldIndex === statuses.indexOf("New") &&
-            index === statuses.indexOf("In progress")) ||
-          (oldIndex === statuses.indexOf("In progress") &&
-            index === statuses.indexOf("Done"))
+          // (oldIndex === statuses.indexOf("New") &&
+          //   index === statuses.indexOf("In progress")) ||
+          // (oldIndex === statuses.indexOf("In progress") &&
+          //   index === statuses.indexOf("Done"))
+          oldIndex < index
         ) {
           order.dates[index] = new Date();
           localStorage.setItem(order.id, JSON.stringify(order.dates));
@@ -120,18 +124,31 @@ function App() {
 
   return (
     <div className="App">
-      <div className="columns-container">
-        {columns.map((column) => (
-          <Column
-            index={column.statusIndex}
-            statuses={statuses}
-            key={column.status}
-            text={column.status}
-            orders={column.orders}
-            changeStatus={changeStatus}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<div />} />
+          <Route
+            path="/kitchen"
+            element={
+              <KitchenPage
+                columns={columns.slice(0, 3)}
+                statuses={statuses.slice(0, 3)}
+                changeStatus={changeStatus}
+              />
+            }
           />
-        ))}
-      </div>
+          <Route
+            path="/waiter"
+            element={
+              <WaiterPage
+                columns={columns.slice(2, statuses.length)}
+                statuses={statuses}
+                changeStatus={changeStatus}
+              />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/kitchenPage.js
@@ -0,0 +1,17 @@
+import Column from "../components/column";
+export default function KitchenPage(props) {
+  return (
+    <div className="columns-container">
+      {props.columns.map((column) => (
+        <Column
+          index={column.statusIndex}
+          statuses={props.statuses}
+          key={column.status}
+          text={column.status}
+          orders={column.orders}
+          changeStatus={props.changeStatus}
+        />
+      ))}
+    </div>
+  );
+}
