05c0a61818f2e571dd6cf1313e0cd7f3254c4ce8
Calvin Zhen <418224@student.fontys.nl>
2022-12-16 10:08:14 +0100

    Updated test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "@testing-library/jest-dom";
-import { render, screen } from '@testing-library/react';
-import App from '../App';
+import { render, screen } from "@testing-library/react";
+import KitchenPage from "../pages/KitchenPage";
 
-test('Renders the columns New, In progress and Done', () => {
-  render(<App />);
+test("Renders the columns New, In progress and Done", () => {
+  render(<KitchenPage />);
   const linkElementNew = screen.getByText(/New/i);
   const linkElementInProgress = screen.getByText(/In progress/i);
-  const linkElementDone = screen.getByText(/Done/i);
+  const linkElementDone = screen.getByText(/Ready/i);
   expect(linkElementNew).toBeInTheDocument();
   expect(linkElementInProgress).toBeInTheDocument();
   expect(linkElementDone).toBeInTheDocument();
