2498d6d9523eb0f91af81b8afd9cf4b345a30de4
Calvin Zhen <418224@student.fontys.nl>
2022-12-16 10:41:11 +0100

    Updated test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,7 +4,21 @@ import { render, screen } from "@testing-library/react";
 import KitchenPage from "../pages/KitchenPage";
 
 test("Renders the columns New, In progress and Done", () => {
-  render(<KitchenPage />);
+  const statuses = ["New", "In progress", "Ready", "Delivered"];
+  let columns = [];
+  statuses.forEach((status, index) =>
+    columns.push({
+      statusIndex: index,
+      status: status,
+      orders: [],
+    })
+  );
+  render(
+    <KitchenPage
+      columns={columns.slice(0, 3)}
+      statuses={statuses.slice(0, 3)}
+    />
+  );
   const linkElementNew = screen.getByText(/New/i);
   const linkElementInProgress = screen.getByText(/In progress/i);
   const linkElementDone = screen.getByText(/Ready/i);
